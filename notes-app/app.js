4dae42df834251e524525927e0fa9b4802ad1f8e
HKHALDI <hkhaldi@ford.com>
2021-09-17 14:03:16 -0400

    importing Node.js core modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
-//import { writeFileSync } from 'fs';
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+
+// fs.writeFileSync('notes.txt', 'My name is Heikel!');
+fs.appendFileSync('notes.txt', ' I live in Detroit.');
+// Chanllenge: Append a message to notes.txt
+//
+// 1. Use appendFileSync to append to the file
+// 2. Run the script.
+// 3. Check your work by opening the file and viewing the appended text.
