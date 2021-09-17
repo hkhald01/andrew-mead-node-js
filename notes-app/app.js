b67e399d05fa23ea0f796c016f9823fe84e55961
HKHALDI <hkhaldi@ford.com>
2021-09-17 15:52:54 -0400

    end of secion 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,13 @@
-const add = require('./utils');
+const validator = require('validator');
+const chalk = require('chalk');
 const notes = require('./notes');
 
-const sum = add(4, -2);
-console.log(sum);
+const msg = notes();
 
-const note = notes();
+console.log(chalk.blue(msg));
 
-console.log(note);
+console.log(chalk.white.bgBlue(validator.isEmail('heikel.khaldi@gmail.com')));
+
+console.log(chalk.yellow.bgBlue.inverse(validator.isURL('https://www.google.com')));
+
+console.log(chalk.magenta.bold('SUCCESS'));
