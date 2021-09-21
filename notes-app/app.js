a757a6398208e49290bff388f3eadb950f52652b
HKHALDI <hkhaldi@ford.com>
2021-09-21 08:48:02 -0400

    section 4 crud notes into a file system using nodejs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,89 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const notes = require('./notes');
 
-const msg = notes();
+// console.log(process.argv);
 
-console.log(chalk.blue(msg));
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(chalk.white.bgBlue(validator.isEmail('heikel.khaldi@gmail.com')));
+//create add command
 
-console.log(chalk.yellow.bgBlue.inverse(validator.isURL('https://www.google.com')));
+yargs.command({
+ command: 'add',
+ describle: 'Add a new note',
+ builder: {
+  title: {
+   describe: 'Note title',
+   demandOption: true,
+   type: 'string',
+  },
+  body: {
+   describe: 'Note body',
+   demandOption: true,
+   type: 'string',
+  },
+ },
+ handler: function (argv) {
+  console.log('Title: ' + argv.title);
+  console.log('Body: ' + argv.body);
+ },
+});
 
-console.log(chalk.magenta.bold('SUCCESS'));
+//create remove command
+
+yargs.command({
+ command: 'remove',
+ describle: 'Remove a note',
+ handler: function () {
+  console.log('Removing a note!');
+ },
+});
+
+//create list command
+
+yargs.command({
+ command: 'list',
+ describle: 'List all notes',
+ handler: function () {
+  console.log('Listing all notes!');
+ },
+});
+
+//create read command
+
+yargs.command({
+ command: 'read',
+ describle: 'Read a note',
+ handler: function () {
+  console.log('Reading a note!');
+ },
+});
+
+// yargs.parse();
+
+// add, remove, read, list
+
+console.log(yargs.argv);
+
+// const command = process.argv[2];
+// if (command === 'add') {
+//  console.log('Adding note');
+// } else if (command === 'remove') {
+//  console.log('Removing note!');
+// }
+
+// const msg = notes();
+
+// console.log(chalk.blue(msg));
+
+// console.log(chalk.white.bgBlue(validator.isEmail('heikel.khaldi@gmail.com')));
+
+// console.log(
+//  chalk.yellow.bgBlue.inverse(validator.isURL('https://www.google.com'))
+// );
+
+// console.log(chalk.magenta.bold('SUCCESS'));
+
+// console.log(process.argv);
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,38 @@
+const fs = require('fs');
 const getNotes = function () {
  return 'Your notes...';
 };
-module.exports = getNotes;
+const addNote = function (title, body) {
+ const notes = loadNotes();
+ const duplicateNotes = notes.filter(function (note) {
+  return note.title === title;
+ });
+ if (duplicateNotes.length === 0) {
+  notes.push({
+   title: title,
+   body: body,
+  });
+  savedNotes(notes);
+  console.log('New note added successfully');
+ } else {
+  console.log('Note title taken!');
+ }
+};
+
+const savedNotes = function (notes) {
+ const dataJSON = JSON.stringify(notes);
+ fs.writeFileSync('notes.json');
+};
+
+const loadNotes = function () {
+ try {
+  const data = JSON.parse(fs.readFileSync('notes.json'));
+  return data;
+ } catch (e) {
+  return [];
+ }
+};
+module.exports = {
+ getNotes: getNotes,
+ addNote: addNote,
+};
