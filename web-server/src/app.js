5987349029ae603730b0e5484ab12b49c9b77d1f
HKHALDI <hkhaldi@ford.com>
2021-10-03 10:48:45 -0400

    templateting with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,31 @@
 const path = require('path');
 const express = require('express');
+
+
 const app = express();
 
+//define paths for Express config
+const publicDirectoryPath = path.join(__dirname, 'public');
+const viewsPath = path.join(__dirname, 'views');
+
+// Setup handlebars engine and views location
+app.set('views', viewsPath);
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '../public')));
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
  res.render('index', { title: 'Weather App', name: 'Andrew Mead' });
 });
 
+app.get('/about', (req, res) => {
+ res.render('about', { title: 'about Me ', name: 'Andrew Mead' });
+});
+
+app.get('/help', (req, res) => {
+ res.render('help', { title: 'Help', text: 'Page help' });
+});
 app.get('/weather', (req, res) => {
  res.send({ location: 'Detroit', forecast: 53 });
 });
