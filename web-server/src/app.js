7967ec4f860cf8b2c782a28e27154b7709d58d69
HKHALDI <hkhaldi@ford.com>
2021-10-03 22:14:37 -0400

    using templates fragments

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
-
+const hbs = require('hbs');
 
 const app = express();
 
 //define paths for Express config
 const publicDirectoryPath = path.join(__dirname, 'public');
-const viewsPath = path.join(__dirname, 'views');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('views', viewsPath);
 app.set('view engine', 'hbs');
+hbs.registerPartials(partialPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -24,7 +26,12 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/help', (req, res) => {
- res.render('help', { title: 'Help', text: 'Page help' });
+ res.render('help', {
+  title: 'Help',
+  text: 'Page help',
+  title: 'Help ',
+  name: 'Andrew Mead',
+ });
 });
 app.get('/weather', (req, res) => {
  res.send({ location: 'Detroit', forecast: 53 });
