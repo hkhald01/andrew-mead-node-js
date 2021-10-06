f8ce96786644cc3df680c35336a45964204eccfb
HKHALDI <hkhaldi@ford.com>
2021-10-05 22:14:09 -0400

    checkpoint section-9

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,7 @@
 console.log('client side javascript file is loaded!');
 
+//commented method
+
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //  response.json().then((data) => {
 //   console.log(data);
@@ -16,7 +18,7 @@ weatherForm.addEventListener('submit', (event) => {
  messageOne.textContent = 'Loading ...';
  messageTwo.textContent = '';
  const location = search.value;
- const url = `http://localhost:3000/weather?address=${location}}`;
+ const url = `/weather?address=${location}}`;
  fetch(url).then((response) => {
   response.json().then((data) => {
    if (data.error) {
